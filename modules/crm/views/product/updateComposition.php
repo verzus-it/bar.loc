bad6ac3d4a611aeb8585f843fb27fde357456062
Сергей <team@verzus-it.com>
2021-02-08 23:46:23 +0200

    Update updateComposition.php

---
+++ modules/crm/views/product/updateComposition.php
@@ -0,0 +1,14 @@
+<form class="form-horizontal">
+	<div class="form-group">
+		<label for="ingridientID" class="col-lg-6 control-label">Ингридиент</label>
+		<div class="col-lg-6">
+
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="amountInProduct" class="col-lg-6 control-label">Количество в продукте, мл.(г.):</label>
+		<div class="col-lg-6">
+			<input type="number" class="form-control input-sm" id="amountInProduct" placeholder="100">
+		</div>
+	</div>
+</form>
