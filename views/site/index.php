d903c5e4c6a945618a3da678215ae403ab607c76
Сергей <team@verzus-it.com>
2021-03-19 12:53:24 +0200

    Тык

--- views/site/index.php
+++ views/site/index.php
@@ -24,6 +24,144 @@ $this->registerCssFile('@web/css/pages/index.css');
 	</div>
 </div>
 
+<div class="wrap">
+	<div class="container">
+		<div class="bestSellers">
+			
+			<div class="bestSellersHead">
+				Топ продажу
+			</div>
+			
+			<div class="bestSellerItem">
+				<img src="images/products/1.jpg" class="productImg" alt="">
+				<div class="title">
+					Long Island Ice Tea
+				</div>
+				<div class="description">
+					Солодкий ягідний твіст на класичну Маргариту. Готуємо зі свіжої полуниці, лаймового фрешу та текіли.
+				</div>
+				<select name="option" class="optionsList">
+					<option value="1">100 мл</option>
+					<option value="2">700 мл</option>
+				</select>
+				<div class="price">120 грн</div>
+				<a href="#" class="button dark">Замовити</a>
+			</div>
+			<div class="bestSellerItem">
+
+				<img src="images/products/2.jpg" class="productImg" alt="">
+				<div class="title">
+					Cosmopolitan
+				</div>
+				<div class="description">
+					Солодкий ягідний твіст на класичну Маргариту. Готуємо зі свіжої полуниці, лаймового фрешу та текіли.
+				</div>
+				<select name="option" class="optionsList">
+					<option value="1">100 мл</option>
+					<option value="2">700 мл</option>
+				</select>
+				<div class="price">130 грн</div>
+				<a href="#" class="button dark">Замовити</a>
+			</div>
+			<div class="bestSellerItem">
+
+				<img src="images/products/3.jpg" class="productImg" alt="">
+				<div class="title">
+					Tequila Sunrise
+				</div>
+				<div class="description">
+					Солодкий ягідний твіст на класичну Маргариту. Готуємо зі свіжої полуниці, лаймового фрешу та текіли.
+				</div>
+				<select name="option" class="optionsList">
+					<option value="1">100 мл</option>
+					<option value="2">700 мл</option>
+				</select>
+				<div class="price">120 грн</div>
+				<a href="#" class="button dark">Замовити</a>
+			</div>
+			<div class="bestSellerItem">
+
+				<img src="images/products/1.jpg" class="productImg" alt="">
+				<div class="title">
+					Long Island Ice Tea
+				</div>
+				<div class="description">
+					Солодкий ягідний твіст на класичну Маргариту. Готуємо зі свіжої полуниці, лаймового фрешу та текіли.
+				</div>
+				<select name="option" class="optionsList">
+					<option value="1">100 мл</option>
+					<option value="2">700 мл</option>
+				</select>
+				<div class="price">120 грн</div>
+				<a href="#" class="button dark">Замовити</a>
+			</div>
+			<div class="bestSellerItem">
+				<img src="images/products/1.jpg" class="productImg" alt="">
+				<div class="title">
+					Long Island Ice Tea
+				</div>
+				<div class="description">
+					Солодкий ягідний твіст на класичну Маргариту. Готуємо зі свіжої полуниці, лаймового фрешу та текіли.
+				</div>
+				<select name="option" class="optionsList">
+					<option value="1">100 мл</option>
+					<option value="2">700 мл</option>
+				</select>
+				<div class="price">120 грн</div>
+				<a href="#" class="button dark">Замовити</a>
+			</div>
+			<div class="bestSellerItem">
+
+				<img src="images/products/2.jpg" class="productImg" alt="">
+				<div class="title">
+					Cosmopolitan
+				</div>
+				<div class="description">
+					Солодкий ягідний твіст на класичну Маргариту. Готуємо зі свіжої полуниці, лаймового фрешу та текіли.
+				</div>
+				<select name="option" class="optionsList">
+					<option value="1">100 мл</option>
+					<option value="2">700 мл</option>
+				</select>
+				<div class="price">130 грн</div>
+				<a href="#" class="button dark">Замовити</a>
+			</div>
+			<div class="bestSellerItem">
+
+				<img src="images/products/3.jpg" class="productImg" alt="">
+				<div class="title">
+					Tequila Sunrise
+				</div>
+				<div class="description">
+					Солодкий ягідний твіст на класичну Маргариту. Готуємо зі свіжої полуниці, лаймового фрешу та текіли.
+				</div>
+				<select name="option" class="optionsList">
+					<option value="1">100 мл</option>
+					<option value="2">700 мл</option>
+				</select>
+				<div class="price">120 грн</div>
+				<a href="#" class="button dark">Замовити</a>
+			</div>
+			<div class="bestSellerItem">
+
+				<img src="images/products/1.jpg" class="productImg" alt="">
+				<div class="title">
+					Long Island Ice Tea
+				</div>
+				<div class="description">
+					Солодкий ягідний твіст на класичну Маргариту. Готуємо зі свіжої полуниці, лаймового фрешу та текіли.
+				</div>
+				<select name="option" class="optionsList">
+					<option value="1">100 мл</option>
+					<option value="2">700 мл</option>
+				</select>
+				<div class="price">120 грн</div>
+				<a href="#" class="button dark">Замовити</a>
+			</div>
+		</div>
+	</div>
+</div>
+
 <script>
     document.addEventListener("DOMContentLoaded", function(){
         $(".slidesPanel > div:gt(0)").hide();
